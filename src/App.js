5a7cc4726eb6844f522e46c3b3e4bd388e1beb6c
Lara Bucher <bucher.lara@web.de>
2022-10-19 20:28:12 +0200

    create react Quizz-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Card from "./Components/Card/index.js";
+import Navbar from "./Components/Navbar/index.js";
+import "./global.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="header">
+        <h1 className="header__title">Quiz-App</h1>
       </header>
+
+      <main>
+        <ul className="card-list"></ul>
+        <Card />
+      </main>
+      {/* <Navbar /> */}
     </div>
   );
 }
